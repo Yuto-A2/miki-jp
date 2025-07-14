79a010c58fb5e4f8f277fa42fc68526a5858f5e1
Yuto Arimori <n01648493@humber.ca>
2025-07-14 11:33:55 -0400

    Changed class image.

--- src/components/ClassImg.tsx
+++ src/components/ClassImg.tsx
@@ -4,10 +4,10 @@ export default function ClassImg() {
     return (
         <>
             {/* background image */}
-            <div className="lg:w-[80%] h-150 bg-[url('/onlineClass.jpg')] bg-cover bg-center mt-3 mx-auto relative">
-                <p className="absolute text-white text-lg left-1/2 -translate-x-1/2 top-20">The free trial class online.</p>
+            <div className="md:w-[80%] w-[90%] h-100 bg-[url('/onlineClass.jpg')] bg-cover bg-center mt-3 mx-auto relative">
+                <p className="absolute text-white font-bold text-lg left-1/2 -translate-x-1/2 top-10">The free trial class online.</p>
                 <Link href="#contact">
-                    <button className="absolute bg-gray-300 left-1/2 -translate-x-1/2 p-2 lg:top-32 top-40 cursor-pointer hover:opacity-80">Get Started for Free</button>
+                    <button className="absolute bg-gray-300 left-1/2 -translate-x-1/2 p-2 lg:top-20 top-30 cursor-pointer hover:opacity-80">Get Started for Free</button>
                 </Link>
             </div>
         </>
